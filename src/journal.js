88e5a0b1f9db919b902c8d2d1374987c46bb0e0a
Annie Shin and Nick Brown <annie@>
2018-07-30 11:31:48 -0700

    add getTeaser function to return first 8 words

--- src/journal.js
+++ src/journal.js
@@ -1,17 +1,28 @@
 export function journal(entry) {
-
+  entry.split("")
   var vowels = ["a", "e", "i", "o", "u"];
   var vowelCount = 0;
-  var constantCount = 0
+  var consonantCount = 0
 
   for (var i=0;i<= entry.length;i++){
     if(entry[i] === "a" || entry[i] === "e" || entry[i] === "i" || entry[i] === "o" || entry[i] === "u"){
       vowelCount+=1;
     }
     else {
-      constantCount +=1
+      consonantCount +=1
     }
   }
+  return "Vowel count:  " + vowelCount + "   Consonant Count: " + consonantCount;
+}
 
-  return "Vowel count:  " + vowelCount + "   Constant Coount: " + constantCount;
+export function getTeaser(words) {
+  var wordSplit = words.split(" ")
+  var results = []
+
+  for (var x=0; x<= 8; x++) {
+    results.push(wordSplit[x])
+  }
+  var results = results.join(" ")
+
+  return results;
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { journal } from './journal';
+import { getTeaser } from './journal';
 
 $(document).ready(function() {
   $('#journal-form').submit(function(event) {
     event.preventDefault();
-    var entry = $('#entry').val().split("");
+    var entry = $('#entry').val();
     var output = journal(entry);
+    var wordOutput = getTeaser(entry);
+    $('#solution').append("<h3>" + wordOutput + "</h3>");
     $('#solution').append("<li>" + output + "</li>");
   });
 });
